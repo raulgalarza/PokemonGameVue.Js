f7f9ec2349773a6b4aed152f2a371f0257811967
Raúl Emilio Desena Galarza <raul.desena@one.gob.do>
2022-10-11 18:46:34 -0400

    Corregido clase pokemon

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -4,12 +4,12 @@
         <img v-if="!showPokemon" 
                 :src="imgSrc"
                 class="hidden-pokemon"
-                alt="pokemon">
+                alt="pokemon clasePokemon">
 
         <img v-else
                 :src="imgSrc"
                 class="fade-in"
-                alt="pokemon">        
+                alt="pokemon clasePokemon">        
     </div>
 </template>
 
